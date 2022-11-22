17bc129a5e1e9849edb6223a6f3d94965ab4fae4
dodoshosh <idos52856@gmail.com>
2022-11-22 18:54:05 +0200

    creating the queries injection

--- baseClass.py
+++ baseClass.py
@@ -7,3 +7,21 @@ class baseClass:
         raise NotImplementedError()
     def result(self):
         raise NotImplementedError()
+class SQL(baseClass):
+    def injection(self):
+        File_object = open(r"sql_queries.txt", "r+")
+        file_data=File_object.read()
+        queries_list=file_data.split('^')
+        for sql_querie in queries_list:
+            print('\n',sql_querie,'\n')
+class XSS(baseClass):
+    def injection(self):
+        File_object = open(r"-------", "r+")   #needs to get the file first!!!!
+        file_data=File_object.read()
+        queries_list=file_data.split('^')
+        for sql_querie in queries_list:
+            print('\n',sql_querie,'\n')
+
+obj=baseClass("a")
+sqlobj=SQL(obj)
+sqlobj.injection()
