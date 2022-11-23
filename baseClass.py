cfd7ca6a909656e781a3ac16da3510547573b8e0
YonadavM <yonadav120@gmail.com>
2022-11-23 23:07:52 +0200

    Update baseClass.py
     - added Enters between classes
    
     - added file name to xss class

--- baseClass.py
+++ baseClass.py
@@ -1,26 +1,35 @@
 class baseClass:
-    def __init__(self,URL):
+    def __init__(self, URL):
         self.URL = URL
+
     def scanning(self):
         raise NotImplementedError()
+
     def injection(self):
         raise NotImplementedError()
+
     def result(self):
         raise NotImplementedError()
+
+
 class SQL(baseClass):
     def injection(self):
         File_object = open(r"sql_queries.txt", "r+")
-        file_data=File_object.read()
-        queries_list=file_data.split('^')
+        file_data = File_object.read()
+        queries_list = file_data.split('^')
         for sql_querie in queries_list:
-            print('\n',sql_querie,'\n')
+            print('\n', sql_querie, '\n')
+
+
 class XSS(baseClass):
     def injection(self):
-        File_object = open(r"-------", "r+")   #needs to get the file first!!!!
-        file_data=File_object.read()
-        queries_list=file_data.split('^')
+        File_object = open(r"xss_queries.txt", "r+")
+        file_data = File_object.read()
+        queries_list = file_data.split('^')
         for sql_querie in queries_list:
-            print('\n',sql_querie,'\n')
-obj=baseClass("a")
-sqlobj=SQL(obj)
+            print('\n', sql_querie, '\n')
+
+
+obj = baseClass("a")
+sqlobj = SQL(obj)
 sqlobj.injection()
