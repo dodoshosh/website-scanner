9bb802c5e87659b06974f8f9ba749f614863e9f7
YonadavM <yonadav120@gmail.com>
2022-11-30 16:23:13 +0200

    fix the queries file

--- Main.py
+++ Main.py
@@ -1,8 +1,12 @@
 from SqlClass import SQL
-import XssClass as XC
+from XssClass import XSS
 from baseClass import baseClass
 
 if __name__ == "__main__":
     obj = baseClass("a")
     sqlobj = SQL(obj)
     sqlobj.injection()
+
+    obj = baseClass("a")
+    xssobj = XSS(obj)
+    xssobj.injection()
