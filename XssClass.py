48a6b6cc74e1de212594e4b767c857f80f01c472
YonadavM <yonadav120@gmail.com>
2022-11-30 18:42:55 +0200

    Update XssClass.py
     - Implemented the scanning method

--- XssClass.py
+++ XssClass.py
@@ -1,7 +1,17 @@
 from baseClass import baseClass
+from selenium.webdriver.common.by import By
 
 
 class XSS(baseClass):
+
+    def scanning(self):
+        print("Scanning...")
+        baseClass.public_DRIVER.get() #TODO: add the user's URL
+        inputsToInject = baseClass.public_DRIVER.find_elements(By.XPATH, "//input[@type=\"text\" or @type=\"search\" or "
+                                                                    "@type=\"email\" or @type=\"password\" or @type=\"url\"]")
+                                                                                       # specifcly made for the input type we need
+        return inputsToInject
+
     def injection(self):
         File_object = open(r"xss_queries.txt", "r+")
         file_data = File_object.read()
