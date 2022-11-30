10d9425e2d4439a96655a9ce1b22adebe500d4ee
YonadavM <yonadav120@gmail.com>
2022-11-30 18:19:10 +0200

    Update SqlClass.py
     - Scanning method created

--- SqlClass.py
+++ SqlClass.py
@@ -1,7 +1,18 @@
 from baseClass import baseClass
+from selenium.webdriver.common.by import By
 
 
 class SQL(baseClass):
+
+    def scanning(self):
+        print("Scanning...")
+        baseClass.public_DRIVER.get() #TODO: add the user's URL
+        inputsToInject = baseClass.public_DRIVER.find_elements(By.XPATH, "//input[@type=\"text\" or @type=\"search\" or "
+                                                                            "@type=\"email\" or @type=\"password\" or @type=\"url\"]")
+                                                                                                #specifcly made for the input type we need
+
+
+
     def injection(self):
         File_object = open(
             r"C:\Users\magshimim\\Documents\magshimim\\Web Vulns Scanner\\Shimon\hadera-804-shimon\\sql_queries.txt", "r+")
