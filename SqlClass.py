5fc4fef53da219e8851f1ff77fd72c2735ff6123
YonadavM <yonadav120@gmail.com>
2022-12-19 16:46:03 +0200

    Update SqlClass.py
     - delete commend because its done

--- SqlClass.py
+++ SqlClass.py
@@ -9,7 +9,7 @@ class SQL(baseClass):
         self.URL = URL
     def scanning(self):
         print("Scanning...")
-        baseClass.public_DRIVER.get(self.URL) #TODO: add the user's URL
+        baseClass.public_DRIVER.get(self.URL)
         inputsToInject = baseClass.public_DRIVER.find_elements(By.XPATH, "//input[@type=\"text\" or @type=\"search\" or "
                                                                          "@type=\"email\" or @type=\"password\" or @type=\"url\"]")
         #specifcly made for the input type we need
