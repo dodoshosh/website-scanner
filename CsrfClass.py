0cbac3d0b7dc819713427ea2f849cb4734d50584
YonadavM <yonadav120@gmail.com>
2023-01-04 19:56:49 +0200

    Update CsrfClass.py
     - made a function to recognize where to  use the injection

--- CsrfClass.py
+++ CsrfClass.py
@@ -8,3 +8,11 @@ from selenium.common import exceptions
 class CSRF(baseClass):
     def __init__(self,URL):
         self.URL = URL
+    def scanning(self):
+        print("Scanning...")
+        inputsToInject = baseClass.public_DRIVER.find_elements(By.XPATH, "//input[@type=\"text\" or @type=\"search\" or "
+                                                                    "@type=\"email\" or @type=\"password\" or @type=\"url\"]"
+																		 								" | //form | //button")
+        baseClass.public_DRIVER.get(self.URL)
+                                                                                       # specifcly made for the input type we need
+        return inputsToInject
