f64f5a2bed2e538ac266ca2aa64f75db56e9a1ef
YonadavM <yonadav120@gmail.com>
2023-01-04 17:44:50 +0200

    Update CiClass.py
     - made an algorithm that checks where we can inject the queries

--- CiClass.py
+++ CiClass.py
@@ -8,3 +8,10 @@ from selenium.common import exceptions
 class CI(baseClass):
     def __init__(self,URL):
         self.URL = URL
+    def scanning(self):
+        print("Scanning...")
+        baseClass.public_DRIVER.get(self.URL)
+        inputsToInject = baseClass.public_DRIVER.find_elements(By.XPATH, "//input[@type=\"text\" or @type=\"search\" or "
+                                                                    "@type=\"email\" or @type=\"password\" or @type=\"url\"]")
+                                                                                       # specifcly made for the input type we need
+        return inputsToInject
