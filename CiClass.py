87c74ca296211578db5ad36d9d3db2d2fc4478b6
YonadavM <yonadav120@gmail.com>
2023-01-10 20:46:52 +0200

    Update CiClass.py

--- CiClass.py
+++ CiClass.py
@@ -15,3 +15,31 @@ class CI(baseClass):
                                                                     "@type=\"email\" or @type=\"password\" or @type=\"url\"]")
                                                                                        # specifcly made for the input type we need
         return inputsToInject
+
+    def injection(self):
+        File_object = open(r".\ci_queries.txt", "r+")
+        file_data = File_object.read()
+        queries_list = file_data.split('^')
+
+        inputsToInject = self.scanning()
+
+        for i in range(len(inputsToInject)):
+            for ci_query in queries_list:
+                try:
+                    inputToInj = inputsToInject[i]
+                    inputToInj.clear()
+                    inputToInj.send_keys(ci_query)
+                    baseClass.public_DRIVER.find_element(By.XPATH, "//input[@type='submit']").click()
+                    successed = self.checkSuccess()
+
+                    if successed:
+                        print("Vuln Founded\n")
+                        return
+
+                except exceptions.StaleElementReferenceException:
+                    inputsToInject = self.scanning()
+
+
+
+    def checkSuccess(self):
+        return #Will be implemented in the next weeks
