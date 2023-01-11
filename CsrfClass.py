9b634a17a62c9d1c7881a6d361d9598385c00015
YonadavM <yonadav120@gmail.com>
2023-01-11 17:34:51 +0200

    Update CsrfClass.py
     - update the class to scan and find csrf vuln

--- CsrfClass.py
+++ CsrfClass.py
@@ -2,20 +2,36 @@ from baseClass import baseClass
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common import exceptions
-
+import requests
 
 
 class CSRF(baseClass):
-    def __init__(self,URL):
+    def __init__(self, URL):
         self.URL = URL
+
     def scanning(self):
-        print("Scanning...")
-        inputsToInject = baseClass.public_DRIVER.find_elements(By.XPATH, "//input[@type=\"text\" or @type=\"search\" or "
-                                                                    "@type=\"email\" or @type=\"password\" or @type=\"url\"]"
-																		 								" | //form | //button")
-        baseClass.public_DRIVER.get(self.URL)
-                                                                                       # specifcly made for the input type we need
-        return inputsToInject
-        
+        res = requests.get(self.URL)
+        headers = res.headers
+        return headers  # In CSRF we check the headers instead of injecting
+
+    def injection(self):
+        File_object = open(r".\csrf_headers.txt", "r+")
+        file_data = File_object.read()
+        headers_list = file_data.split('^')
+
+        headersToCheck = self.scanning()
+
+        for i in range(len(inputsToInject)):
+            for header in headers_list:
+                try:
+                    successed = self.checkSuccess(header, headersToCheck)
+
+                    if successed:
+                        print("Vuln Founded\n")
+                        return
+
+                except exceptions.StaleElementReferenceException:
+                    inputsToInject = self.scanning()
+
     def checkSuccess(self):
-        return #Will be implemented in the next weeks
+        return  # Will be implemented in the next weeks
